60c4c84cb63f89004a8177daa1ac6203ebb26510
Luiza Corpaci <corpaci.luiza@yahoo.com>
2024-07-07 21:48:19 +0300

    update with aws timings

--- Exercise 3/client_aws.py
+++ Exercise 3/client_aws.py
@@ -3,25 +3,87 @@ import sys
 import uuid
 import boto3
 import time
+import pandas as pd
 
 def upload_image_to_s3(image_path, bucket_name):
+    '''
+    Upload an image to an S3 bucket and return the image ID'''
     s3 = boto3.client('s3')
     image_id = str(uuid.uuid4())
     
     t1_client = time.time()  # Start measuring client transfer time
     s3.upload_file(image_path, bucket_name, image_id)
-    t2_client = time.time()  # End measuring client transfer time
+    t1_server = time.time()  # End measuring client transfer time
     
-    with open("output/aws_client_TT.txt", "a") as f:
-        f.write(f"{image_id}, {t1_client}, {t2_client}\n")
+    with open("output/aws_t1.txt", "a") as f:
+        f.write(f"{image_id}, {t1_client}, {t1_server}\n")
 
     return image_id
 
-def get_lambda_processing_time(image_id):
-    # Implement a method to retrieve Lambda processing time, e.g., from logs or a database
-    # This is a placeholder implementation.
-    lambda_processing_time = 0.5  # Replace with actual logic
-    return lambda_processing_time
+
+def get_timings(image_id):
+    '''
+    Retrieve the Lambda processing time from the DynamoDB table'''
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('timings_table_dic2024s') 
+    response = table.query(
+        KeyConditionExpression=boto3.dynamodb.conditions.Key('ImageID').eq(image_id)
+    )
+    
+    timeout = 20 # Timeout in seconds
+    timeout_time = time.time()
+    while time.time() - timeout_time < timeout:
+        time.sleep(1)
+        response = table.query(
+            KeyConditionExpression=boto3.dynamodb.conditions.Key('ImageID').eq(image_id)
+        )
+        if response['Items']:
+            timings = response['Items'][0]
+            t2_server = float(timings['t2_server'])
+            print(f"Lambda processing time for ImageID {image_id}.")
+            return t2_server
+    else:
+        print(f"Timeout: No timings found for ImageID {image_id} within {timeout} seconds")
+        return -1
+
+def get_results(image_id):
+    '''
+    Retrieve the Lambda results from the DynamoDB table'''
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('yolo_results_dic2024s') 
+    response = table.query(
+        KeyConditionExpression=boto3.dynamodb.conditions.Key('ImageID').eq(image_id)
+    )
+    
+    timeout = 20 # Timeout in seconds
+    timeout_time = time.time()
+    while time.time() - timeout_time < timeout:
+        time.sleep(1)
+        response = table.query(
+            KeyConditionExpression=boto3.dynamodb.conditions.Key('ImageID').eq(image_id)
+        )
+        if response['Items']:
+            print(f"Accessed image with id={image_id}")
+            return True
+    else:
+        print(f"Timeout: No timings found for ImageID {image_id} within {timeout} seconds")
+        return False
+
+
+
+def export_table_to_csv(table_name):
+    '''
+    Export the DynamoDB table to a CSV file
+    '''
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table(table_name)
+
+    response = table.scan()
+    data = response['Items']
+    df = pd.DataFrame(data)
+    df.to_csv(f'output\\{table_name}.csv', index=False)
+    print(f"Data exported successfully to {table_name}.csv")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -35,8 +97,17 @@ if __name__ == "__main__":
         image_path = os.path.join(input_folder, filename)
         image_id = upload_image_to_s3(image_path, bucket_name)
         print(f"Uploaded {filename} as {image_id}")
+        if get_results(image_id):
+            t2_client = time.time()
+            t2_server = get_timings(image_id)
+            with open("output/aws_t2.txt", "a") as f:
+                f.write(f"{image_id}, {t2_client}, {t2_server}\n")
 
-        # Measure and log the Lambda processing time
-        lambda_processing_time = get_lambda_processing_time(image_id)
-        with open("output/aws_lambda_processing_time.txt", "a") as f:
-            f.write(f"{image_id}, {lambda_processing_time}\n")
+
+    export_table_to_csv('yolo_results_dic2024s')
+    export_table_to_csv('timings_table_dic2024s')
+
+
+
+
+
--- Exercise 3/dic2024s-docker-repo/lambda_function.py
+++ Exercise 3/dic2024s-docker-repo/lambda_function.py
@@ -7,29 +7,21 @@ from urllib.parse import unquote_plus
 
 s3 = boto3.client('s3')
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('yolo_results_dic2024s')
-
-# Variables for Average Inference Time
-countImages = 0
-runtime = 0
-avgTime = 0
+results_table = dynamodb.Table('yolo_results_dic2024s')
+timings_table = dynamodb.Table('timings_table_dic2024s')
 
 def lambda_handler(event, context):
-    global countImages, runtime, avgTime
-    
-    t1_server = time.time()  # Start measuring Lambda execution time
-    
-    # Load YOLO model and labels
-    labels = open('/opt/yolo_tiny_configs/coco.names').read().strip().split("\n")
-    net = cv2.dnn.readNetFromDarknet('/opt/yolo_tiny_configs/yolov3-tiny.cfg', '/opt/yolo_tiny_configs/yolov3-tiny.weights')
-    layer_names = net.getLayerNames()
-    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
-
     for record in event['Records']:
+        # t2_client = time.time()
+        
+        # Load YOLO model and labels
+        labels = open('/opt/yolo_tiny_configs/coco.names').read().strip().split("\n")
+        net = cv2.dnn.readNetFromDarknet('/opt/yolo_tiny_configs/yolov3-tiny.cfg', '/opt/yolo_tiny_configs/yolov3-tiny.weights')
+        layer_names = net.getLayerNames()
+        output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+
         s3_bucket = record['s3']['bucket']['name']
         s3_key = unquote_plus(record['s3']['object']['key'])
-        s3_url = f"s3://{s3_bucket}/{s3_key}"
-
         try:
             image_object = s3.get_object(Bucket=s3_bucket, Key=s3_key)
             image_content = image_object['Body'].read()
@@ -39,7 +31,6 @@ def lambda_handler(event, context):
 
         image = cv2.imdecode(np.frombuffer(image_content, np.uint8), cv2.IMREAD_COLOR)
         
-        countImages += 1  # Increment image count
         start_time = time.time()  # Start measuring inference time
 
         # YOLO
@@ -58,30 +49,26 @@ def lambda_handler(event, context):
                     results.append({"label": class_label, "accuracy": str(confidence)})
 
         inference_time = time.time() - start_time  # Measure inference time
+        print(f"Inference Time: {inference_time}")
 
-        # Update average inference time
-        runtime += inference_time
-        avgTime = runtime / countImages
-        print(f"Average Inference Time: {avgTime}")
+        t2_server = time.time()  # End measuring Lambda execution time
 
-        # Log the inference time
-        with open("/tmp/lambda_inference_time.txt", "a") as f:
-            f.write(f"{s3_key}, {inference_time}, {avgTime}\n")
-        
-        # DynamoDB
-        table.put_item(
+        results_table.put_item(
             Item={
-                'ImageID': s3_key,  # Add ImageID key
-                'ImageURL': s3_url,
+                'ImageID': s3_key, 
                 'DetectedObjects': results
             }
         )
 
-    t2_server = time.time()  # End measuring Lambda execution time
-    
-    # Log the execution time
-    with open("/tmp/lambda_execution_time.txt", "a") as f:
-        f.write(f"{s3_key}, {t1_server}, {t2_server}\n")
+
+        timings_table.put_item(
+            Item={
+                'ImageID': s3_key,
+                'InferenceTime': str(inference_time),
+                # 't2_client': str(t2_client),
+                't2_server': str(t2_server)
+            }
+        )
     
     return {
         'statusCode': 200,
